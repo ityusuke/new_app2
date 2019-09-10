7fb48248e80aee3977c38896f4aa97914158bff1
EC2 Default User <ec2-user@ip-172-31-18-221.us-east-2.compute.internal>
2019-09-10 11:25:50 +0000

    use gmaps4rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,26 @@
 //= require jquery3
 //= require jquery_ujs
 //= require turbolinks
-//= require underscore
-//= require gmaps/google
 //= require popper
 //= require bootstrap-sprockets
+//= require underscore
+//= require gmaps/google
 //= require_tree .
+
+ handler = Gmaps.build('Google');
+handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+  markers = handler.addMarkers([
+    {
+      "lat": 0,
+      "lng": 0,
+      "picture": {
+        "url": "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
+        "width":  32,
+        "height": 32
+      },
+      "infowindow": "hello!"
+    }
+  ]);
+  handler.bounds.extendWith(markers);
+  handler.fitMapToBounds();
+});
