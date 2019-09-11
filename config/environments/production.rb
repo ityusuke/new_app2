66423152c700af4426e8cfadab9ba699a80dcb54
EC2 Default User <ec2-user@ip-172-31-18-221.us-east-2.compute.internal>
2019-09-11 11:47:30 +0000

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@
 Rails.application.configure do
   config.assets.initialize_on_precompile = false
   # Settings specified here will take precedence over those in config/application.rb.
-  rails credentials:edit
 config.require_master_key = true
   # Code is not reloaded between requests.
   config.cache_classes = true
